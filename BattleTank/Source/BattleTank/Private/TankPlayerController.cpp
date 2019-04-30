325fc6913b87dc9cc89b079075a34b9b9311a6e3
Caris <caris@artthouglorious.com>
2019-04-29 21:17:05 -0700

    Added some logging to the cpp classes to show they are working

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,10 +3,23 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
-
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessed tank: %s"), *(ControlledTank->GetName()));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController has not possessed a tank"));
+	}
+}
 
 ATank * ATankPlayerController::GetControlledTank() const
 {
+	
+	
+
 	return Cast<ATank>(GetPawn());
 }
 
